f899317346afb655491051dba18e328f61c547e8
marbaek <girayjohnmarvic09@gmail.com>
2025-08-19 13:30:10 +0800

    changes

--- db/timeline_milestones.sql
+++ db/timeline_milestones.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Aug 17, 2025 at 03:26 PM
+-- Host: 127.0.0.1
+-- Generation Time: Aug 19, 2025 at 07:29 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,17 +43,10 @@ CREATE TABLE `timeline_milestones` (
 --
 
 INSERT INTO `timeline_milestones` (`id`, `timeline_id`, `title`, `description`, `deadline`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Proposal Submission', '', '2023-12-23 00:23:00', 'pending', '2025-08-17 11:05:59', '2025-08-17 11:05:59'),
-(2, 2, 'Proposal Submission', '', '2025-12-23 12:00:00', 'pending', '2025-08-17 11:11:02', '2025-08-17 11:11:02'),
-(3, 3, 'Proposal Submission', '', '2025-08-18 12:00:00', 'pending', '2025-08-17 11:20:39', '2025-08-17 11:20:39'),
-(4, 4, 'Pre-Submission Stage', 'Week 1–2\r\n\r\nOrientation for research guidelines & policies\r\n\r\nDistribution of research handbook/templates\r\n\r\nInitial consultation with faculty/adviser', '2025-08-19 12:00:00', 'pending', '2025-08-17 12:14:02', '2025-08-17 12:14:02'),
-(5, 4, 'Submission & Review', 'Week 3–5\r\n\r\nSubmission of Research Titles (students submit at least 3 suggested titles)\r\n\r\nTitle Screening by CRAD / Department Committee\r\n\r\nApproved title forwarded for adviser assignment', '2025-08-19 12:00:00', 'pending', '2025-08-17 12:14:02', '2025-08-17 12:14:02'),
-(6, 4, 'Defense Stage', 'Week 9–10\r\n\r\nPanel Assignment by CRAD/Department\r\n\r\nProposal defense schedule finalized\r\n\r\nStudents defend proposal before panel\r\n\r\nPanel recommendations documented', '2025-08-28 12:00:00', 'pending', '2025-08-17 12:14:02', '2025-08-17 12:14:02'),
-(7, 5, 'Proposal Submission', 'gawin mo na ', '2025-08-17 00:00:00', 'pending', '2025-08-17 12:29:16', '2025-08-17 12:29:16'),
-(8, 5, 'Revision', 'Gawin mo na ', '2025-08-18 12:00:00', 'pending', '2025-08-17 12:29:16', '2025-08-17 12:29:16'),
-(9, 5, 'Defense ', 'Sheesh', '2025-08-20 12:00:00', 'pending', '2025-08-17 12:29:16', '2025-08-17 12:29:16'),
-(10, 6, 'Proposal Submission', '', '2025-08-26 12:00:00', 'pending', '2025-08-17 12:39:58', '2025-08-17 12:39:58'),
-(11, 7, 'Proposal Submission', '', '2025-08-27 12:00:00', 'pending', '2025-08-17 13:14:03', '2025-08-17 13:14:03');
+(28, 13, 'Chapter 1 Week 1', '', '2025-08-19 13:21:00', 'pending', '2025-08-19 05:19:43', '2025-08-19 05:19:43'),
+(29, 13, 'Chapter 2 Week 2', '', '2025-08-19 13:22:00', 'pending', '2025-08-19 05:19:43', '2025-08-19 05:19:43'),
+(30, 14, 'Chapter 1 Week 1', '', '2025-08-19 13:27:00', 'pending', '2025-08-19 05:26:13', '2025-08-19 05:26:13'),
+(31, 14, 'Chapter 2 Week 2', '', '2025-08-19 13:28:00', 'pending', '2025-08-19 05:26:13', '2025-08-19 05:26:13');
 
 --
 -- Indexes for dumped tables
@@ -74,17 +67,7 @@ ALTER TABLE `timeline_milestones`
 -- AUTO_INCREMENT for table `timeline_milestones`
 --
 ALTER TABLE `timeline_milestones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `timeline_milestones`
---
-ALTER TABLE `timeline_milestones`
-  ADD CONSTRAINT `timeline_milestones_ibfk_1` FOREIGN KEY (`timeline_id`) REFERENCES `submission_timelines` (`id`) ON DELETE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
